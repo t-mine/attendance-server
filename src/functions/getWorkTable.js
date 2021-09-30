a8fc441df096e88ccac27d0e0ca886ee543272c4
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-09-30 21:24:44 +0900

    勤務表テーブル追加

---
+++ src/functions/getWorkTable.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// ワークテーブル取得API
+module.exports.handler = async (event, context) => {
+  // リクエストボディ
+  const requestBody = JSON.parse(event.body);
+  console.log(event.body);
+
+  // 検索条件を作成
+  const params = {
+    TableName: 'work-table',
+    Key: { email: requestBody.email },
+  };
+
+  // ユーザーテーブルを検索
+  const result = await dynamoDb.get(params).promise();
+  const user = result.Item;
+  console.log('検索結果 : ' + JSON.stringify(user));
+
+  // レスポンスを生成
+  let statusCode = 401;
+  let body = '';
+  if (user && user.password === requestBody.password) {
+    statusCode = 200;
+    body = JSON.stringify(user);
+  }
+  const response = {
+    statusCode: statusCode,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: body,
+  };
+
+  // レスポンスを返す
+  context.succeed(response);
+};
