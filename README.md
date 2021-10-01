b2a1eb196eb3f553d2e4e41b00096ebde2011b4f
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-10-01 23:40:42 +0900

    勤務表検索API修正

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ serverless deploy function -f login --stage dev --verbose
 ```
 curl -X POST -kv https://XXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/dev/login -d "{\"email\":\"tanaka@example.com\",\"password\":\"tanaka\"}"
 curl -X DELETE -kv https://XXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/dev/logout
+curl -X GET -kv "https://XXXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/dev/work-table?email=tanaka@example.com&year=2021&month=9"
 ```
 
 ## Remove resources
--- src/functions/getWorkTable.js
+++ src/functions/getWorkTable.js
@@ -5,28 +5,37 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 // ワークテーブル取得API
 module.exports.handler = async (event, context) => {
-  // リクエストボディ
-  const requestBody = JSON.parse(event.body);
-  console.log(event.body);
+  // クエリ文字列
+  // ※クエリ文字列はリクエストボディと違いJSON形式文字列ではなく
+  //   オブジェクトとしてeventに格納されているためパース不要
+  console.log(event);
+  const requestParam = event.queryStringParameters;
 
   // 検索条件を作成
   const params = {
-    TableName: 'work-table',
-    Key: { email: requestBody.email },
+    TableName: 'work_table',
+    // TODO たぶん、GSIの検索の仕方があるので調べる
+    Key: {
+      email: requestParam.email,
+      year_month: requestParam.year + requestParam.month,
+    },
   };
 
   // ユーザーテーブルを検索
-  const result = await dynamoDb.get(params).promise();
-  const user = result.Item;
-  console.log('検索結果 : ' + JSON.stringify(user));
+  let workTable;
+  try {
+    const result = await dynamoDb.get(params).promise();
+    workTable = result.Item;
+    console.log('検索結果 : ' + JSON.stringify(workTable));
+  } catch (e) {
+    console.log('DynamoDB検索失敗');
+    console.log(e);
+    // TODO ここで失敗用のレスポンスを返したい。
+  }
 
   // レスポンスを生成
-  let statusCode = 401;
-  let body = '';
-  if (user && user.password === requestBody.password) {
-    statusCode = 200;
-    body = JSON.stringify(user);
-  }
+  let statusCode = 200;
+  let body = JSON.stringify(workTable);
   const response = {
     statusCode: statusCode,
     headers: { 'Access-Control-Allow-Origin': '*' },
